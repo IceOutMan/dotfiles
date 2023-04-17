5332c047ed48ee215ffaff37a85d229b1acc8d51
meiken.gu <aluo_fei@163.com>
2023-04-17 20:32:29 +0800

    add nvim config
    add nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,53 @@
+require("init-packer")
+require("init-lsp")
+
+-- common config
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.termguicolors = true
+vim.o.cursorline = true
+
+vim.g.mapleader = ' '
+
+-- 快速保存、退出、加载配置文件
+vim.cmd('noremap Q :q<CR>')
+vim.cmd('noremap S :w<CR>')
+vim.cmd('noremap R :source $INITVIM<CR>')
+
+-- 快速光标移动
+vim.cmd('noremap J 5j')
+vim.cmd('noremap K 5k')
+vim.cmd('noremap L 5l')
+vim.cmd('noremap H 5h')
+
+-- Tab Change
+vim.cmd('noremap tn :tabe<CR>')
+vim.cmd('noremap th :-tabnext<CR>')
+vim.cmd('noremap tl :+tabnext<CR>')
+
+-- Screen Size Change
+vim.cmd('noremap <C-k> :res +5<CR>')
+vim.cmd('noremap <C-j> :res -5<CR>')
+vim.cmd('noremap <C-h> :vertical resize+5<CR>')
+vim.cmd('noremap <C-l> :vertical resize-5<CR>')
+
+-- Screen Skip
+vim.cmd('noremap <LEADER>h <C-w>h')
+vim.cmd('noremap <LEADER>l <C-w>l')
+vim.cmd('noremap <LEADER>j <C-w>j')
+vim.cmd('noremap <LEADER>k <C-w>k')
+
+-- Screen Split
+vim.cmd('noremap sl :set splitright<CR>:vsplit<CR>')
+vim.cmd('noremap sh :set nosplitright<CR>:vsplit<CR>')
+vim.cmd('noremap sk :set nosplitbelow<CR>:split<CR>')
+vim.cmd('noremap sj :set splitbelow<CR>:split<CR>')
+
+
+-- Nvim tree qucik 
+vim.cmd('map tt :NvimTreeToggle<CR>')
+vim.cmd('map <LEADER>r :QuickRun<CR>')
---
+++ nvim/lua/init-packer.lua
@@ -0,0 +1,80 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- surround
+  use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  })
+
+  -- auto pair
+  use {
+	"windwp/nvim-autopairs",
+    config = function() 
+        require("nvim-autopairs").setup {} 
+    end
+  }
+
+  -- status line
+  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+  -- tab line
+  use 'beauwilliams/statusline.lua'
+
+  -- comment
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+        require('Comment').setup()
+    end
+  }
+  -- nvim tree
+  use 'nvim-tree/nvim-web-devicons'
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+        'nvim-tree/nvim-web-devicons', -- optional
+    },
+    config = function()
+        require("nvim-tree").setup {}
+    end
+  }
+  
+  -- lsp config
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+
+  -- code formater
+  use 'sbdchd/neoformat'
+  -- code runner
+  use { 'michaelb/sniprun', run = 'bash ./install.sh' }
+  use 'thinca/vim-quickrun'
+
+
+
+  -- color scheme
+  use { 'navarasu/onedark.nvim',
+    config = function()
+        require('onedark').load()
+    end
+  }
+
+  -- git
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+        require('gitsigns').setup()
+    end
+  }
+
+  -- use 'NTBBloodbath/doom-one.nvim'
+
+  end)
