7b6e57ee5a2f3dfd0d877b98921d26c3c95316d9
IceOutMan <ainewbeginglf@gmail.com>
2023-09-04 22:28:48 +0800

    add: telescope.nvim

--- nvim/lua/config/plugins/telescope.lua
+++ nvim/lua/config/plugins/telescope.lua
@@ -1,11 +1,9 @@
--- surround
 return {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
+    { 'nvim-lua/plenary.nvim' },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    },
 }
