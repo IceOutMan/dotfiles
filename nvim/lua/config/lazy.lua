44c535f6e1006839d63f7b8ea9474d8aee6f70fd
meiken.gu <aluo_fei@163.com>
2023-09-07 16:04:49 +0800

    modify: update lazy.nvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,4 +1,5 @@
--- macOS/Linux: ~/.local/share/nvim
+-- macOS/Linux: ~/.local/share/nvim is vim.fn.stdpath("data")
+-- lazypath ->  ~/.local/share/nvim/lazy/lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
